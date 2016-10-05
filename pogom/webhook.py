1ea59b0a647ea3248bfe3d18734877168e432053
pir2 <pir2@users.noreply.github.com>
2016-10-05 16:32:15 -0400

    add webhook_overseer_thread(args, wh_queue, enc_ids_done, position):

--- pogom/webhook.py
+++ pogom/webhook.py
@@ -4,14 +4,15 @@
 import logging
 import requests
 from .utils import get_args
-
+from .models import Pokemon
+from .search import get_new_coords
 log = logging.getLogger(__name__)
 
 
 def send_to_webhook(message_type, message):
     args = get_args()
 
-    if not args.webhooks:
+    if not args.webhooks and not args.webhook_db_only:
         # what are you even doing here...
         return
 
@@ -42,3 +43,65 @@ def wh_updater(args, q):
                 q.task_done()
         except Exception as e:
             log.exception('Exception in wh_updater: %s', e)
+
+def webhook_overseer_thread(args, wh_queue, enc_ids_done, position):
+
+    log.info('Webhook overseer starting')
+
+    parse_lock = Lock()
+    threadStatus = {}
+
+    current_location = [position[0],position[1]]
+    wh_pokemonids = args.webhook_ids
+    current_location = args.location
+
+    #threadStatus['Overseer'] = {}
+    #threadStatus['Overseer']['message'] = "Initializing"
+    #threadStatus['Overseer']['type'] = "Overseer"
+    #threadStatus['Overseer']['method'] = "Webook"
+
+    # Create a search_worker_thread per account
+    log.info('Starting search worker threads')
+
+    # A place to track the current location
+
+    # The real work starts here but will halt on pause_bit.set()
+    # get the webhook area - borrowed from spawnpoint_only
+    sp_dist = 0.07 * 2 * args.step_limit
+    log.debug('Spawnpoint search radius: %f', sp_dist)
+    # generate coords of the midpoints of each edge of the square
+    south, west = get_new_coords(current_location, sp_dist, 180), get_new_coords(current_location, sp_dist, 270)
+    north, east = get_new_coords(current_location, sp_dist, 0), get_new_coords(current_location, sp_dist, 90)
+
+    swLat, swLng = south, west
+    neLat, neLng = north, east
+    
+    while True:
+        #get pokemon that are disappearing in the future
+
+        #place pokemon into queue for webhook
+        
+        for p in Pokemon.get_active_by_id(wh_pokemonids, swLat, swLng,
+                         neLat, neLng) 
+                         and p['encounter_id'] not in enc_ids_done['encounter_id']:
+            wh_update_queue.put(('pokemon', {
+                'encounter_id': p['encounter_id'],
+                'spawnpoint_id': p['spawn_point_id'],
+                'pokemon_id': ['pokemon_id'],
+                'latitude': p['latitude'],
+                'longitude': p['longitude'],
+                'disappear_time': p['disappear_time'],
+                'last_modified_time': p['last_modified_timestamp_ms'],
+                'time_until_hidden_ms': p['time_till_hidden_ms']
+            }))        
+            #add encounter id to enc_ids_done = {}
+            enc_ids_done.append(p)
+        
+        #clean up old pokemon
+        new_enc_ids = [for old in enc_ids_done if old['disappear_time'] > datetime.utcnow()]
+        enc_ids_done = new_enc_ids
+        
+        #pause for 30s
+        time.sleep(30)
+
+
