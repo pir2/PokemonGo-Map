17e7dd34bd0de1cf91b10830b57d5ba28d3a6843
pir2 <pir2@users.noreply.github.com>
2016-10-05 16:41:21 -0400

    add action='append', default=[] to whids

--- pogom/utils.py
+++ pogom/utils.py
@@ -158,7 +158,7 @@ def get_args():
     parser.add_argument('-el', '--encrypt-lib', help='Path to encrypt lib to be used instead of the shipped ones')
     parser.add_argument('-whdbonly','--webhook-db-only', help='Requires webhook. Grab pokemons from DB and send to webhook, no searcher, no server',
                         action='store_true', default=False)
-    parser.add_argument('-whids','--webhook-ids', help='Only sends specified pokemon ids to webhook',
+    parser.add_argument('-whids','--webhook-ids', action='append', default=[], help='Only sends specified pokemon ids to webhook',
                         action='store_true', default=False)    
     parser.set_defaults(DEBUG=False)
 
